7a0c9df62375f6b0f9ec3897e830c8d6d2bf187d
ead <ead@academico.local>
2025-05-21 19:46:39 -0300

    dia 21

--- atividade_automacao/main.py
+++ atividade_automacao/main.py
@@ -1,18 +1,25 @@
 #crie um program que comita o prokero de voces e envie o repositório para o github
-import pyautogui as auto 
-
-auto.PAUSE = 2
+import pyautogui as p
 
 
-auto.press("win")
-auto.typewrite("vscode")
-auto.press("enter")
-auto.hotkey("ctrl", "j")
-auto.typewrite("git status")
-auto.press("enter")
-auto.typewrite("git add .")
-auto.press("enter")
-auto.typewrite("git commit -m 'atividade automacao'")
-auto.press("enter")
-auto.typewrite("git push")
-auto.press("enter")
+
+def limpa_credciais():
+    p.write("git config --global --unset user.name")
+    p.press("enter")
+    p.write("git config --global --unset user.email")
+    p.press("enter")
+
+if __name__ == "__main__":
+    p.PAUSE = 0.5
+msg_commit = input("Digite a mensagem do commit: ")
+
+limpa_credciais()
+
+p.typewrite("git status")
+p.press("enter")
+p.typewrite("git add .")
+p.press("enter")
+p.typewrite(f"git commit -m '{msg_commit}'")
+p.press("enter")
+p.typewrite("git push")
+p.press("enter")
